e9975cc561b9a16a5eaaf76efa3bb5abfb625fd4
youngsoohan <amazingmobdev@gmail.com>
2019-12-29 23:04:15 +0900

    static

--- express/src/app.js
+++ express/src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.send("hello homepage");
 });
 
 app.get("/login", (req, res) => {
   res.send("<strong>Login please!</strong>");
+});
+
+app.get("/rn", (req, res) => {
+  res.send("<strong>RN SEOUL Image</strong><br /> <img src='/rns.png' />");
 });
 
 app.listen(3000, () => {
