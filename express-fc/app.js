ab38a3e7b78f4bfe23a0da68a0067a0d7a9ce1c6
youngsoohan <amazingmobdev@gmail.com>
2020-04-06 01:55:24 +0900

    middleware

--- express-fc/app.js
+++ express-fc/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const logger = require("morgan")
+
 const admin = require("./routes/admin")
 const app = express();
 const port = 3000
+
+const testMiddleware = (req, res, next) => { 
+  console.log("first middleware!!")
+  next()
+}
 
 app.get("/", (req, res) => { 
   res.send("express start!")
 })
 
-app.use("/admin", admin)
+app.use(logger("dev"))
+
+app.use("/admin",testMiddleware,  admin)
+
 
 app.listen(port, () => { 
   console.log(`express is running on port ${port}`)
